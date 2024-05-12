36ecd9b9ce8bf543f29ce99d74b5a6807ef98517
prashant9428 <pathakprashant841@gmail.com>
2024-05-12 09:45:51 +0530

    feat: added react project

---
+++ frontend/my-first-app/src/App.jsx
@@ -0,0 +1,21 @@
+import List from "./component/List";
+
+
+function App() {
+ const names = ["prashant","sagar","yogesh","Dhanashree","Priyanka"];
+
+ // ! return statement will always have jsx
+ // ! props are when you are passing properties from one componet to another component
+ // ! props are passed in attributes
+    return (
+       <>
+          <List userData={names}/>
+       </>
+    )
+}
+
+// common js --> module.exports = App
+// module --->  export default App
+// export const name = prahant
+
+export default App;
---
+++ frontend/my-first-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' // function component - -> jsx
+
+
+
+
+ReactDOM.createRoot(root).render(  
+    <App />
+)
+// ! you can not pass multiple components in render
+// ! first letter of the component should be capital
